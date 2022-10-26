1a7c02683b123c35be0b1a0f0e3791997278b32a
matiur9 <matiurrahman@MATIURs-MacBook-Pro.local>
2022-10-25 21:59:40 -0400

    homework

---
+++ src/hw1JavaBasic/MySelf.java
@@ -0,0 +1,23 @@
+package hw1JavaBasic;
+
+/*
+ -it is fun when java works
+ -sometimes its hard if program is not running
+ -I will find my way
+ */
+
+public class MySelf {
+
+	// here is the single line method
+	// I show from here
+
+	public static void main(String[] args) {
+
+		System.out.println("My name is Matiur Rahman Khan");
+		System.out.println("I live in Astoria");
+		System.out.println("Originally from Bangladesh");
+		System.out.println("I like to travel");
+
+	}
+
+}
