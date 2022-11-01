18f27bd5931b0a5b8f9ff8aa20ea8ed9452cb2aa
matiur9 <matiurrahman@MATIURs-MacBook-Pro.local>
2022-10-31 21:34:06 -0400

    hw

---
+++ src/hw2JavaBasics/MyCar.java
@@ -0,0 +1,18 @@
+package hw2JavaBasics;
+
+public class MyCar {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Matiur Rahman, I love to drive, My car info is below:");
+				
+		System.out.println("My favourite car is :BMW ");
+		System.out.print("color :Navy blue");
+		System.out.print("\nYear : 2022");
+		System.out.print("\nwheel drive : AWD");
+		System.out.print("\t\nThe car price is :$85000");
+
+	}
+
+}
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author matiurrahman
- *
- */
-module HomeWorkCoreJava {
-}
